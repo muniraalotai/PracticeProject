a6ae68f8461b59630a812c35c8e82d9d1672f875
Munira <muniraaotai@gmail.com>
2021-06-11 18:00:40 +0300

    implement logging using serilog

--- PracticeProject/Repositories/AuthorDbRepository.cs
+++ PracticeProject/Repositories/AuthorDbRepository.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using PracticeProject.Data;
 using PracticeProject.Models;
+using Serilog;
 
 namespace PracticeProject.Repositories
 {
@@ -18,29 +19,34 @@ namespace PracticeProject.Repositories
 
         public IList<Author> List()
         {
+            Log.Information("Get authors list");
             return _dbContext.Authors.ToList();
         }
 
         public Author Find(int Id)
         {
+            Log.Information("Find author by id");
             var author = _dbContext.Authors.SingleOrDefault(a => a.Id == Id);
             return author;
         }
 
         public void Add(Author entity)
         {
+            Log.Information("Add new author");
             _dbContext.Authors.Add(entity);
             _dbContext.SaveChanges();
         }
 
         public void Update(int Id, Author entity)
         {
+            Log.Information("Update existing author");
             _dbContext.Update(entity);
             _dbContext.SaveChanges();
         }
 
         public void Delete(int Id)
         {
+            Log.Information("Delete existing author");
             var author = Find(Id);
             _dbContext.Authors.Remove(author);
             _dbContext.SaveChanges();
@@ -48,6 +54,7 @@ namespace PracticeProject.Repositories
 
         public List<Author> Search(string word)
         {
+            Log.Information("Search for author by word");
             var result = _dbContext.Authors.Where(a=> a.FullName.Contains(word));
             return result.ToList();
         }
--- PracticeProject/Repositories/BookDbRepository.cs
+++ PracticeProject/Repositories/BookDbRepository.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using PracticeProject.Data;
 using PracticeProject.Models;
+using Serilog;
 
 namespace PracticeProject.Repositories
 {
-    public class BookDbRepository: IBookStoreRepository<Book>
+    public class BookDbRepository : IBookStoreRepository<Book>
     {
         public BookStoreDbContext _dbContext;
-        
+
         public BookDbRepository(BookStoreDbContext dbContext)
         {
             this._dbContext = dbContext;
@@ -17,29 +18,34 @@ namespace PracticeProject.Repositories
 
         public IList<Book> List()
         {
+            Log.Information("Get books list");
             return _dbContext.Books.Include(a => a.Author).ToList();
         }
 
         public Book Find(int Id)
         {
+            Log.Information("Find book by id");
             var book = _dbContext.Books.Include(a => a.Author).SingleOrDefault(b => b.Id == Id);
             return book;
         }
 
         public void Add(Book entity)
         {
+            Log.Information("Add new book");
             _dbContext.Books.Add(entity);
             _dbContext.SaveChanges();
         }
 
         public void Update(int Id, Book entity)
         {
+            Log.Information("Update existing book");
             _dbContext.Update(entity);
             _dbContext.SaveChanges();
         }
 
         public void Delete(int Id)
         {
+            Log.Information("Delete existing book");
             var book = Find(Id);
             _dbContext.Books.Remove(book);
             _dbContext.SaveChanges();
@@ -47,10 +53,11 @@ namespace PracticeProject.Repositories
 
         public List<Book> Search(string word)
         {
+            Log.Information("Search for book by word");
             var result = _dbContext.Books.Include(a => a.Author)
                 .Where(b => b.Title.Contains(word)
-                            ||b.Description.Contains(word)
-                            ||b.Author.FullName.Contains(word));
+                            || b.Description.Contains(word)
+                            || b.Author.FullName.Contains(word));
             return result.ToList();
         }
     }
